2c385552904bd4f5f248d604565c86be678b01d2
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-02-05 22:11:06 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sumitroajiprabowo/app-say-thankyou
+
+go 1.17
+
+require github.com/sumitroajiprabowo/go-say-thankyou v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_thankyou "github.com/sumitroajiprabowo/go-say-thankyou"
+)
+
+func main() {
+	fmt.Println(go_say_thankyou.SayThankYou())
+}
